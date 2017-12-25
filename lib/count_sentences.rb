629cca6ace46aa0e55889336faf5763a23ebc744
Yurina Shin <yurinas@gmail.com>
2017-12-25 19:27:58 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    if self.sentence? || self.question? || self.exclamation?
+    if (self.sentence? || self.question?) && (self.question? || self.exclamation?)
       # binding.pry
       self.split(/\.|\!|\?/).count
 

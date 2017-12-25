c5e0e2fa440766d869603b336e59d72aeb2749e5
Yurina Shin <yurinas@gmail.com>
2017-12-25 20:07:37 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    # split on punctuation
+    # delete empty strings
+    # take the count
+    # if the original string didn't end in punctunation
+    #    subtract 1 from count
+    # return max(0, count)
+
+    a = self.split(/\.|\!|\?/)
+    a.delete("")
+    # binding.pry
+    a.count
+
 
   end
 end

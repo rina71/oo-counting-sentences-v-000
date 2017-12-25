4be5d3c18146289b8c44a2869eea4ebe7a56e67f
Yurina Shin <yurinas@gmail.com>
2017-12-25 20:01:09 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -29,10 +29,10 @@ class String
     # if self.sentence? || self.question? || self.exclamation?
     #   binding.pry
     #   self.split(/\.|\!|\?/).count
-    # 
+    #
     # elsif self.split(/\.|\!|\?/).include?("")
     #   a = self.split(/\.|\!|\?/)
-    # 
+    #
     #   a.delete("")
     #   return a.count
     # else
